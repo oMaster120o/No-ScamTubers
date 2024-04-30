3d3f3bd4c19a278eb5cae0db2319eeefa1535bec
oMaster120o <140356933+oMaster120o@users.noreply.github.com>
2024-04-30 17:57:48 -0300

    some filtering

--- src/modules/Report.py
+++ src/modules/Report.py
@@ -3,14 +3,26 @@ from os import remove, rename
 
 report_path: str = "./user/report/"
 
+Hazard_Float: list[float] = [0.0, 0.75, 1.5, 2.25, 3]
+Hazard: list[str] = ["low", "moderate", "high", "insane", "EXTREME"]
 
 def ReportChannel(Channel_ID: str,
                   Video_ID: str,
-                  Danger: int,
+                  Danger: float,
                   Is_Scam: bool,
                   Is_Malware: bool,
                   Message: str,
                   Channel_Name: str):
+
+    for num in range(len(Hazard_Float)):
+        if Danger == Hazard_Float[num]:
+            Danger: str = Hazard[num]
+        else:
+            continue
+
+    if Channel_Name == "":
+        print("Channel name is None: perhaps the URL is not a valid video")
+        return
 
     Report_File: dict = {
             "ch_name": f"{Channel_Name}",
@@ -21,6 +33,7 @@ def ReportChannel(Channel_ID: str,
             "malware": Is_Malware,
             "message": f"{Message}"
             }
+
     with open(f"{Channel_Name}.json", "w") as report:
         json.dump(Report_File, report)
         report.close()
@@ -32,4 +45,5 @@ def ReportChannel(Channel_ID: str,
         print(Exception, "This Report Already Exists\n")
         remove(f"./{Channel_Name}.json")
     finally:
-        return print("Report Script finished\n")
+        print("Report Script finished\n")
+        return
